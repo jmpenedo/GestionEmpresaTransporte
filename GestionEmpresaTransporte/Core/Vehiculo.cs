35042bc8adf88b72c517e685c684731ee0e5b360
Rubén Fernández González <72012377+rfgonzalez13@users.noreply.github.com>
2020-12-08 13:27:32 +0100

    Añadido comprobaciones de fechas en el constructor
    Signed-off-by: rfgonzalez13 <rfgonzalez@esei.uvigo.es>

--- GestionEmpresaTransporte/Core/Vehiculo.cs
+++ GestionEmpresaTransporte/Core/Vehiculo.cs
@@ -7,40 +7,70 @@ namespace GestionEmpresaTransporte.Core
 {
     public abstract class Vehiculo
     {
-        public string Matricula { get; private set; }
+        public string Matricula { get; }
 
-        public string Modelo { get; set; }
+        public string Modelo { get;
 
-        public string Marca { get; set; }
+        public string Marca { get;  }
 
-        public double Consumo { get; set; }
+        public double Consumo { get; set;}
 
-        public DateTime FechaFabricacion { get; set; }
+        public DateTime FechaFabricacion { get; }
 
         public DateTime FechaAdquisicion { get; set; }
 
-        public string Tipo { get; set; }
+        public string Tipo { get; protected set; }
 
-        public float Peso { get; set; }
+        public float Peso { get; protected set; }
 
         public List<string> Comodidades;
 
-        protected Vehiculo(string m, DateTime f, DateTime a, double c, string mar, string mod, List<String> comodidades)
+        /// <summary>
+        ///     Crea un objeto de tipo vehiculo <see cref="Vehiculo" />
+        /// </summary>
+        /// <param name="matricula">El identificador del vehiculo, formato europero 0000XXX</param>
+        /// <param name="fechaFabricacion">Fecha en la cual el vehiculo ha salido de la fábrica</param>
+        /// <param name="fechaAdquisicion">Fecha en la cual el vehiculo ha sido adquirido por la empresa</param>
+        /// <param name="consumo">Consumo de combustible en L/100Km del vehiculo</param>
+        /// <param name="marca">Fabricante del vehiculo</param>
+        /// <param name="modelo">Modelo del vehiculo</param>
+        /// <param name="comodidades">Comodidades de las que dispone el vehiculo</param>
+        protected Vehiculo(string matricula, DateTime fechaFabricacion, DateTime fechaAdquisicion, double consumo, 
+            string marca, string modelo, List<String> comodidades)
         {
-            if (ValidarMatricula(m.ToUpper()))
+            if (utilidades.ValidarMatricula(matricula.ToUpper()))
             {
-                this.Matricula = m.ToUpper();
-                this.Consumo = c;
-                this.FechaAdquisicion = a;
-                this.FechaFabricacion = f;
-                this.Modelo = mod;
-                this.Marca = mar;
-                this.Comodidades = comodidades;
+                this.Matricula = matricula.ToUpper();
             }
             else
             {
                 throw new InvalidDataException("Formato de matrícula incorrecto");
             }
+
+            if (Consumo > 0)
+            {
+                this.Consumo = consumo;  
+            }
+            else
+            {
+                throw new InvalidDataException("El consumo debe se mayor que 0");
+            }
+
+            if (DateTime.Compare(fechaFabricacion, fechaAdquisicion) <= 0)
+            {
+                this.FechaAdquisicion = fechaAdquisicion;
+                this.FechaFabricacion = fechaFabricacion;
+            }
+            else
+            {
+                throw new InvalidDataException("La fecha de fabricacion no puede ser anterior a la de adquisicion");
+            }
+            
+            this.Modelo = modelo;
+            this.Marca = marca;
+            this.Comodidades = comodidades;
+            
+            
         }
 
         public String ComodidadesToString()
@@ -68,18 +98,7 @@ namespace GestionEmpresaTransporte.Core
                 $"\n\tFecha de fabricacion: {this.FechaFabricacion}\nFecha de adquisicion: {this.FechaAdquisicion}";
         }
         
-
-        public static bool ValidarMatricula(string m)
-        {
-           
-            Regex rx = new Regex(@"[0-9]{4}[A-Z]{3}");
-            return rx.IsMatch(m.ToUpper()) & m.Length == 7;
-        }
-
-        public void AñadirComodidades(List<string> c)
-        {
-            Comodidades = c;
-        }
+        
 
         public abstract String GetTipo();
 
