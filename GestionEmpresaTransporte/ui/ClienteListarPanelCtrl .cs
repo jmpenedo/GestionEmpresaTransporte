41ab084a85d6d2f4f3ec1688dfc8bdb77b35aff7
rfgonzalez13 <rfgonzalez@esei.uvigo.es>
2020-12-13 21:43:59 +0100

    Añadido doble click en clientes
    Signed-off-by: rfgonzalez13 <rfgonzalez@esei.uvigo.es>

--- GestionEmpresaTransporte/ui/ClienteListarPanelCtrl .cs
+++ GestionEmpresaTransporte/ui/ClienteListarPanelCtrl .cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.Linq;
 using GestionEmpresaTransporte.Core;
@@ -18,6 +19,8 @@ namespace GestionEmpresaTransporte.ui
             Insertar
         }
 
+        public bool seleccion;
+        
         private readonly BindingList<Cliente> _bindingList;
         private Cliente miCliente;
 
